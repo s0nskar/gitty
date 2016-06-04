0c7cebf6f2d93dcd1476ab53cac65596ad3192d2
Sanskar Modi <sanskarmodi97@gmail.com>
2016-06-04 23:12:15 +0530

    added configration module

---
+++ configration.js
@@ -0,0 +1,20 @@
+const nconf = require('nconf').file({file: getUserHome() + '/gitty.json'});
+
+fuction saveSettings(settingKey, settingValue) {
+  nconf.set(settingKey, settingValue);
+  nconf.save();
+}
+
+function.readSettings(settingKey) {
+  nconf.load();
+  return nconf.get(settingKey);
+}
+
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
+
+module.exports = {
+  saveSettings: saveSettings,
+  readSettings: readSettings
+};
